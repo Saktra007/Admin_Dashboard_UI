0b2f7e8854d09cde445f34c2e016ff375cb7492b
Saktra007 <gamingst410@gmail.com.com>
2026-04-19 16:06:03 +0700

    feat: add missing exports for UI components in index.js

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,2 +1,12 @@
 export { default as Modal } from "./Modal";
-export { default as StatCard } from "./StatCard";
+export { default as Button } from "./Button";
+export { default as Input } from "./Input";
+export { default as Select } from "./Select";
+export { default as DarkModeToggle } from "./DarkModeToggle";
+export { default as Badge } from "./Badge";
+export { default as LoadingScreen } from "./LoadingScreen";
+export { default as EmptyState } from "./EmptyState";
+export { default as StatItem } from "./StatItem";
+export { default as Logo } from "./Logo";
+export { default as AvatarUpload } from "../ui/AvatarUpload";
+export{default as Pagination} from './Pagination'
