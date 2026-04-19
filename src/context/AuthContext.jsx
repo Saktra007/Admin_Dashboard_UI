decebc97d4ec508e38a9f5ab9cac4f5b1fc55be6
Saktra007 <gamingst410@gmail.com.com>
2026-04-19 16:07:30 +0700

    feat: add AuthContext for user authentication management with login, logout, and registration support

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,53 @@
+import { createContext, useEffect, useState } from "react";
+import { userService } from "../services/user.service";
+
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem("userInfo");
+    if (savedUser) setCurrentUser(JSON.parse(savedUser));
+    setLoading(false);
+  }, []);
+
+  const login = async (email, password) => {
+    const res = await userService.login({ email, password });
+
+    const userData = res.data ;
+    if (res.success && userData.token) {
+      setCurrentUser(userData);
+      localStorage.setItem("token", userData.token);
+      localStorage.setItem("userInfo", JSON.stringify(userData));
+      return { success: true };
+    }
+    return { success: false };
+  };
+
+  const registerSuccess = (userData) => {
+    if (userData && userData.token) {
+      setCurrentUser(userData);
+      localStorage.setItem("token", userData.token);
+      localStorage.setItem("userInfo", JSON.stringify(userData));
+      return true;
+    }
+    return false
+  };
+
+  const logout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("userInfo");
+    window.location.href = "/login";
+  };
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login, logout,registerSuccess, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
