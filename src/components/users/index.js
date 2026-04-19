541465b88d1ef01d3deab6ecf1bbf05bd0890b53
Saktra007 <gamingst410@gmail.com.com>
2026-04-19 16:06:55 +0700

    feat: add StatCards, DeleteConfirm, and UserProfile components to user module exports

--- src/components/users/index.js
+++ src/components/users/index.js
@@ -1,3 +1,6 @@
 export { default as UserFilter } from "./UserFilter";
 export { default as UserTable } from "./UserTable";
 export { default as UserForm } from "./UserForm";
+export { default as StatCards } from "./StatCards";
+export { default as DeleteConfirm } from "./DeleteConfirm";
+export { default as UserProfile } from "./UserProfile";
