860202fa66d6919dcfd90578128793473246cf49
Saktra007 <gamingst410@gmail.com.com>
2026-04-19 16:04:30 +0700

    feat: add index file for dashboard components export

---
+++ src/components/dashboard/index.js
@@ -0,0 +1,8 @@
+export { default as DashboardHeader } from "./DashboardHeader";
+export { default as StatCard } from "./StatCard";
+export { default as MainChart } from "./MainChart";
+export { default as StatusDonut } from "./StatusDonut";
+export { default as ActivityHeatmap } from "./ActivityHeatmap";
+export { default as RecentUsers } from "./RecentUsers";
+export { default as TipCard } from "./TipCard";
+export { default as InsightModal } from "./InsightModal";
