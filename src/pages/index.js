d86a1a4dd8501dd83fa77f828b3a7d231fe83b77
Saktra007 <gamingst410@gmail.com.com>
2026-04-19 16:07:55 +0700

    feat: add index file to export main page components

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Dashboard } from "./Dashboard";
+export { default as Login } from "./Login";
+export { default as LoginSuccess } from "./LoginSuccess.jsx";
+export { default as Register } from "./Register";
+export { default as Users } from "./Users";
